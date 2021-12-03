9ba2340b35d89e1cfdadba55367a1083d00e591f
Alexanonymus <91850234+Alexanonymus@users.noreply.github.com>
2021-12-03 16:33:09 +0500

    python manage.py collectstatic qilindi
    170 static files copied

--- booksite/booksite/settings.py
+++ booksite/booksite/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'booksite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,8 +118,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'booksite/static'),]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
