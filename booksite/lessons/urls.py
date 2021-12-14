45a427a20100915d092ab7426463d0c55f77e5f7
Alexanonymus <91850234+Alexanonymus@users.noreply.github.com>
2021-12-14 15:15:24 +0500

    win11 dan kn

--- booksite/lessons/urls.py
+++ booksite/lessons/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('', CategoryListViews.as_view(),name='home'),
+    path('<int:pk>', SubcategoryListViews.as_view(),name='category_detail'),
+    path('lesson/<int:pk>', SubcategoryDetailView.as_view(),name='subcategory_detail'),
 
 ]
--- booksite/lessons/views.py
+++ booksite/lessons/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 
 from .models import *
 # # Create your views here.
@@ -28,4 +28,28 @@ from .models import *
 class CategoryListViews(ListView):
     model = Category
     template_name = 'lessons/base.html'
-    context_object_name =
+    context_object_name = 'categories'
+
+class SubcategoryListViews(DetailView):
+    model = SubCategory
+    template_name = 'lessons/base2.html'
+    context_object_name = 'subcategories'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['subcategory'] = SubCategory.objects.all()
+        context['categories'] = Category.objects.all()
+        return context
+
+
+class SubcategoryDetailView(DetailView):
+    model = SubCategory
+    template_name = 'lessons/lesson.html'
+    context_object_name = 'lessons'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pk = self.kwargs['pk']
+        context['subcategory'] = SubCategory.objects.all()
+        context['categories'] = Category.objects.all()
+        context['lesson'] = Lessons.objects.get(subcategory__id = pk)
+        return context
